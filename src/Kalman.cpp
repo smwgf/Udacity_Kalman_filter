5fdfc446fc5b2e0d580a7018ea9346eeb06bbce2
mason.suh <mason.suh@kakaocorp.com>
2019-10-30 16:43:45 +0900

    [Mason] first commit

---
+++ src/Kalman.cpp
@@ -0,0 +1,113 @@
+﻿// Kalman.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+//#include "pch.h"
+/**
+ * Write a function 'filter()' that implements a multi-
+ *   dimensional Kalman Filter for the example given
+ */
+
+#include <iostream>
+#include <vector>
+#include "Eigen/Dense"
+
+using std::cout;
+using std::endl;
+using std::vector;
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+
+// Kalman Filter variables
+VectorXd x;	// object state
+MatrixXd P;	// object covariance matrix
+VectorXd u;	// external motion
+MatrixXd F; // state transition matrix
+MatrixXd H;	// measurement matrix
+MatrixXd R;	// measurement covariance matrix
+MatrixXd I; // Identity matrix
+MatrixXd Q;	// process covariance matrix
+
+vector<VectorXd> measurements;
+void filter(VectorXd &x, MatrixXd &P);
+
+
+int main() {
+	/**
+	 * Code used as example to work with Eigen matrices
+	 */
+	 // design the KF with 1D motion
+	x = VectorXd(2);
+	x << 0, 0;
+
+	P = MatrixXd(2, 2);
+	P << 1000, 0, 0, 1000;
+
+	u = VectorXd(2);
+	u << 0, 0;
+
+	F = MatrixXd(2, 2);
+	F << 1, 1, 0, 1;
+
+	H = MatrixXd(1, 2);
+	H << 1, 0;
+
+	R = MatrixXd(1, 1);
+	R << 1;
+
+	I = MatrixXd::Identity(2, 2);
+
+	Q = MatrixXd(2, 2);
+	Q << 0, 0, 0, 0;
+
+	// create a list of measurements
+	VectorXd single_meas(1);
+	single_meas << 1;
+	measurements.push_back(single_meas);
+	single_meas << 2;
+	measurements.push_back(single_meas);
+	single_meas << 3;
+	measurements.push_back(single_meas);
+
+	// call Kalman filter algorithm
+	filter(x, P);
+	std::string text;
+	std::cin >> text;
+	cout << text;
+	std::cin >> text;
+	return 0;
+}
+
+
+void filter(VectorXd &x, MatrixXd &P) {
+
+	for (unsigned int n = 0; n < measurements.size(); ++n) {
+
+		VectorXd z = measurements[n];
+		// TODO: YOUR CODE HERE
+
+		// KF Measurement update step
+		VectorXd y = z - H * x;
+		MatrixXd S = H * P*H.transpose() + R;
+		MatrixXd K = P * H.transpose()*S.inverse();
+		// new state
+		x = x + K * y;
+		P = (I - K * H)*P;
+		// KF Prediction step
+		x = F * x + u;
+		P = F * P*F.transpose() + Q;
+
+		cout << "x=" << endl << x << endl;
+		cout << "P=" << endl << P << endl;
+	}
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
